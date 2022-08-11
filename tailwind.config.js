a37a8d3d1d6921dac664476c6e6bd848e172611c
Tchilo <tchiloross@gmail.com>
2022-08-11 17:59:48 +0200

    Add styles for tablet and Desktop view

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,21 @@ module.exports = {
       cyan: '#44D3D2',
       orange: '#FCAE4A',
       blue: '#549EF2',
-      black: '#4D4F62'
+      black: '#4D4F62',
+      white: '#fff'
     },
     fontFamily: {
       sans: ['Poppins', 'sans-serif']
+    },
+    gridTemplateRows: {
+      // Simple 8 row grid
+      '5': 'repeat(5, minmax(0, 125px))',
+
+      // Complex site-specific row configuration
+    },
+    gridTemplateColumns: {
+      '3': 'repeat(3, minmax(0, 350px))',
+      '2': 'repeat(2, minmax(0, 350px))'
     },
     extend: {},
   },
