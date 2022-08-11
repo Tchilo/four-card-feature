8841315feb1d9a9e36b605431dc92b3374c68d27
Tchilo <tchiloross@gmail.com>
2022-08-11 09:07:45 +0200

    Add content and style for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}"
   ],
   theme: {
+    colors: {
+      red: '#EA5454',
+      cyan: '#44D3D2',
+      orange: '#FCAE4A',
+      blue: '#549EF2',
+      black: '#4D4F62'
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
